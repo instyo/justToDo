49ca880c9473d355d07552ebe44edc5889a9b4aa
instyo <nsetyo19@gmail.com>
2025-11-17 01:37:31 +0700

    Initial Commit

---
+++ Just ToDo/Just_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_ToDoApp.swift
+//  Just ToDo
+//
+//  Created by ikhwan on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Just_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
